3b04b87bb6482c5152a09e3979614d0f0a64eda2
reold <redicrafty@gmail.com>
2023-12-28 20:50:42 +0530

    / track downloads

--- main.py
+++ main.py
@@ -45,21 +45,23 @@ async def playlist(id: str):
 
     return playlist_info
 
-@app.get("/download")
-async def download(link: str):
+@app.get("/track")
+async def download_track(link: str, unique_code: str = "", create: bool = False):
 
-    unique_code = uuid().hex
+    if create:
+        unique_code = uuid().hex
+
     sys.argv = [sys.argv[0], "-l", link, "-o", f"./downloads/{unique_code}"]
     
     try:
         spotify_dl()
     except:
-        return JSONResponse({"success": False, "info": "playlist couldn't be downloaded"}, status_code=404)
+        return JSONResponse({"success": False, "info": "track couldn't be downloaded"}, status_code=404)
 
-    folder_name = os.listdir(f"./downloads/{unique_code}")[0]
-    songs = os.listdir(f"./downloads/{unique_code}/{folder_name}")
+    songs = os.listdir(f"./downloads/{unique_code}/")
 
     return JSONResponse({"success": True, "data": {"unique_code": unique_code, "count": len(songs)}})
+
 
 @app.get("/stream")
 async def stream(unique_code: str, background_tasks: BackgroundTasks):
@@ -69,24 +71,28 @@ async def stream(unique_code: str, background_tasks: BackgroundTasks):
     if not os.path.exists(unique_folder_path):
         return JSONResponse({"success": False, "info": "unique resource doesn't exist"}, status_code=404)
     
-    folder_name = os.listdir(f"{unique_folder_path}")[0]
-    songs = os.listdir(f"{unique_folder_path}/{folder_name}")
+    folders = os.listdir(f"{unique_folder_path}")
 
-    if len(songs) == 0:
+    if len(folders) == 0:
         return JSONResponse({"success": False, "info": "no resource to stream"}, status_code=404)
 
+    f_folder_name = folders[0]
+    song_name = os.listdir(f"{unique_folder_path}/{f_folder_name}")[0]
 
-    f_song_name = songs[0]
-    f_song_path = f"{unique_folder_path}/{folder_name}/{f_song_name}"
 
-    file_response = FileResponse(f_song_path)
+    song_path = f"{unique_folder_path}/{f_folder_name}/{song_name}"
+
+    file_response = FileResponse(song_path)
     
-    background_tasks.add_task(pop_unique_cache, f_song_path, folder_name, unique_folder_path)
+    background_tasks.add_task(pop_unique_cache, song_path, unique_folder_path, f_folder_name)
 
     return file_response
 
-def pop_unique_cache(song_path: str, folder_name: str, unique_folder_path: str):
+def pop_unique_cache(song_path, unique_folder_path, folder_name):
     os.remove(song_path)
 
     if len(os.listdir(f"{unique_folder_path}/{folder_name}")) == 0:
+        shutil.rmtree(f"{unique_folder_path}/{folder_name}")
+    
+    if len(os.listdir(unique_folder_path)) == 0:
         shutil.rmtree(unique_folder_path)
