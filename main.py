c5f9a3a3eb111478f166d002a70d8b52bbb4784f
reold <redicrafty@gmail.com>
2024-04-18 14:39:46 +0400

    + more logs

--- main.py
+++ main.py
@@ -115,11 +115,18 @@ def task_deezer_dl(key: str, link: str, id: str):
     failed = False
 
     try:
-        isrc = spotify.track(link)["external_ids"]["isrc"]
+        spotify_track = spotify.track(link)
+        print(f"[TASK DEEZER DL]: spotify track retrieved \n{spotify_track}")
+        
+        isrc = spotify_track["external_ids"]["isrc"]
+        print(f"[TASK DEEZER DL]: Downloading isrc {isrc}")
+
         deezer_dl = DeezerDownloader()
         print("[TASK DEEZER DL]: Download started")
         deezer_dl.download(isrc, id_path)
     except:
+        print("[TASK DEEZER DL]: Download failed successfully")
+        
         failed = True
         shutil.rmtree(f"{id_path}")
     
